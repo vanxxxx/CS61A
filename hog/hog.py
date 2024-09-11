bd58b6b1b2f01154d482b3cac5ac8b4a0e322de5
Vanx <1595891876@qq.com>
2024-09-11 21:48:21 +0800

    comlpete 11

--- hog/hog.py
+++ hog/hog.py
@@ -354,6 +354,8 @@ def boar_strategy(score, opponent_score, threshold=11, num_rolls=6):
 def sus_strategy(score, opponent_score, threshold=11, num_rolls=6):
     """This strategy returns 0 dice when your score would increase by at least threshold."""
     # BEGIN PROBLEM 11
+    if sus_update(0,score,opponent_score) >= (score + threshold):
+        return 0
     return num_rolls  # Remove this line once implemented.
     # END PROBLEM 11
 
