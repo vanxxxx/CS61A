df543ab02434d7dee6b75cf60937c10d7f95c316
vanxxxx <14242035+vanxxxx@user.noreply.gitee.com>
2024-09-28 23:20:11 +0800

    can't do question make_anonymous_factorial but cv answer

--- hw02-20/hw02.py
+++ hw02-20/hw02.py
@@ -175,5 +175,4 @@ def make_anonymous_factorial():
     >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
     True
     """
-    return 'YOUR_EXPRESSION_HERE'
-
+    return (lambda f: lambda n: f(f, n))(lambda f, n: 1 if n == 1 else mul(n, f(f, sub(n, 1))))
