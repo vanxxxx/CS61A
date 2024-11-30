72e48dccfba8a598530df405c9c9b7361472d999
vanxxxx <14242035+vanxxxx@user.noreply.gitee.com>
2024-11-30 15:28:12 +0800

    finish optional1

--- ants/ants.py
+++ ants/ants.py
@@ -104,6 +104,7 @@ class Ant(Insect):
     food_cost = 0
     is_container = False
     is_doubled = False
+    blocks_path = True
     # ADD CLASS ATTRIBUTES HERE
 
     def __init__(self, health=1):
@@ -497,14 +498,18 @@ class NinjaAnt(Ant):
     name = "Ninja"
     damage = 1
     food_cost = 5
+    blocks_path = False
     # OVERRIDE CLASS ATTRIBUTES HERE
     # BEGIN Problem Optional 1
-    implemented = False  # Change to True to view in the GUI
+    implemented = True  # Change to True to view in the GUI
     # END Problem Optional 1
 
     def action(self, gamestate):
         # BEGIN Problem Optional 1
         "*** YOUR CODE HERE ***"
+        for bees in self.place.bees[:]:
+            if bees:
+                bees.reduce_health(self.damage)
         # END Problem Optional 1
 
 
@@ -571,7 +576,8 @@ class Bee(Insect):
         """Return True if this Bee cannot advance to the next Place."""
         # Special handling for NinjaAnt
         # BEGIN Problem Optional 1
-        return self.place.ant is not None
+
+        return self.place.ant is not None and self.place.ant.blocks_path is True
         # END Problem Optional 1
 
     def action(self, gamestate):
