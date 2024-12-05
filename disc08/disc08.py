c8d6efd0bc276dbf51942bf1b6728fc7f3cce4b5
vanxxxx <14242035+vanxxxx@user.noreply.gitee.com>
2024-12-05 17:52:55 +0800

    try disc 08 but not understand how to do

---
+++ disc08/disc08.py
@@ -0,0 +1,44 @@
+def strange_loop():
+    """
+    >>> s = strange_loop
+    >>> s.rest.first.rest = s
+    """
+    s = Link(6, Link(Link(1)))
+    s.rest.first.rest = s
+    return s
+
+
+class Link:
+    """A linked list is either a Link object or Link.empty
+
+    >>> s = Link(3, Link(4, Link(5)))
+    >>> s.rest
+    Link(4, Link(5))
+    >>> s.rest.rest.rest is Link.empty
+    True
+    >>> s.rest.first * 2
+    8
+    >>> print(s)
+    <3 4 5>
+    """
+
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest:
+            rest_repr = ", " + repr(self.rest)
+        else:
+            rest_repr = ""
+        return "Link(" + repr(self.first) + rest_repr + ")"
+
+    def __str__(self):
+        string = "<"
+        while self.rest is not Link.empty:
+            string += str(self.first) + " "
+            self = self.rest
+        return string + str(self.first) + ">"
