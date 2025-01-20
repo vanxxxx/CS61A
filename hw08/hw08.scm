690f5421c88175a5f4b0b956a519ca87e01f6c8b
vanxxxx <1595891876@qq.com>
2025-01-20 20:41:39 +0800

    finish q3

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -12,6 +12,8 @@
       (my-filter pred (cdr s)))
   ))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+  (if (null? lst1) lst2 (cons (car lst1) (interleave lst2 (cdr lst1))) ) 
+  )
 
 (define (no-repeats s) 'YOUR-CODE-HERE)
