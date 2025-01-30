e8f33effd72c74d24b67ea8e7fb5b0fd264bd126
vanxxxx <1595891876@qq.com>
2025-01-30 18:33:57 +0800

    finish q5

--- scheme/scheme_forms.py
+++ scheme/scheme_forms.py
@@ -71,6 +71,8 @@ def do_quote_form(expressions, env):
     validate_form(expressions, 1, 1)
     # BEGIN PROBLEM 5
     "*** YOUR CODE HERE ***"
+
+    return expressions.first
     # END PROBLEM 5
 
 
